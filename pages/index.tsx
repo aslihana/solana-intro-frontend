f7272a680b1d79a54039f82548d4c25286e390af
Aslihan Akbiyik <aslihan.akbiyik@mezun.deu.edu.tr>
2022-12-04 16:13:02 +0300

    first ship

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import * as web3 from '@solana/web3.js'
 import type { NextPage } from 'next'
 import { useState } from 'react'
 import styles from '../styles/Home.module.css'
@@ -10,17 +11,23 @@ const Home: NextPage = () => {
 
   const addressSubmittedHandler = (address: string) => {
     try {
+    
       setAddress(address)
-      const key = new Web3.PublicKey(address)
-      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+      const key = new web3.PublicKey(address);
+      //setAddress(key.toBase58())
+  
+      const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
+  
       connection.getBalance(key).then(balance => {
-        setBalance(balance / Web3.LAMPORTS_PER_SOL)
+        setBalance(balance / web3.LAMPORTS_PER_SOL)
       })
-    } catch (error) {
-      setAddress('')
-      setBalance(0)
-      alert(error)
     }
+      catch(error) {
+        setAddress('')
+        setBalance(0)
+        alert(error)
+      }
+  
   }
 
   return (
